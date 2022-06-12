1de7afba85b4bd2aad63750c68ee4dd87336ba45
Kniffen <knifftech@gmail.com>
2022-06-12 15:11:15 +0200

    Added interactionCreate event handler

--- discord/client.js
+++ discord/client.js
@@ -1,8 +1,9 @@
 import DiscordJS from 'discord.js'
 
-import onError        from './eventHandlers/onError.js'
-import onReady        from './eventHandlers/onReady.js'
-import onGuildCreate  from './eventHandlers/onGuildCreate.js'
+import onError             from './eventHandlers/onError.js'
+import onReady             from './eventHandlers/onReady.js'
+import onGuildCreate       from './eventHandlers/onGuildCreate.js'
+import onInteractionCreate from './eventHandlers/onInteractionCreate.js'
 
 if (!process.env.DISCORD_TOKEN)
   throw new Error('Missing Discord token')
@@ -21,8 +22,9 @@ const client = new DiscordJS.Client({
   ]
 })
 
-client.on('error',       (err)    => onError(err))
-client.on('ready',       (client) => onReady(client))
-client.on('guildCreate', (guild)  => onGuildCreate(guild))
+client.on('error',             (err)         => onError(err))
+client.on('ready',             (client)      => onReady(client))
+client.on('guildCreate',       (guild)       => onGuildCreate(guild))
+client.on('interactionCreate', (interaction) => onInteractionCreate(interaction))
 
 export default client
---
+++ discord/eventHandlers/onInteractionCreate.js
@@ -0,0 +1,22 @@
+import * as modules from '../../modules/index.js'
+
+/**
+ * Handler for Discord client's interaction create event.
+ * 
+ * @param {Object} interaction - Discord interaction object
+ * @returns {Promise<void>}
+ */
+export default async function onInteractionCreate(interaction) {
+  if (interaction.type != 'APPLICATION_COMMAND') return
+
+  for (const mod of Object.values(modules)) {
+    if (!mod.commands) continue
+    
+    const command = Object.values(mod.commands).find(cmd => cmd.data.name == interaction.commandName)
+
+    if (command) {
+      await command.execute(interaction).catch(console.error)
+      break
+    }
+  }
+}
