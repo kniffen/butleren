90d6c1397477ccc9112b4f6ab23e4959c6851fff
Kniffen <knifftech@gmail.com>
2022-06-12 15:19:01 +0200

    Added info module

---
+++ modules/info/commands/ping.js
@@ -0,0 +1,14 @@
+import { SlashCommandBuilder } from '@discordjs/builders'
+
+export const isLocked = true
+export const data =
+  new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Latency check')
+
+/**
+ * @param {Object} interaction - Discord interaction object.
+ */
+export async function execute(interaction) {
+  interaction.reply('pong!')
+}
---
+++ modules/info/index.js
@@ -0,0 +1,6 @@
+export const id = 'info'
+export const name = 'Info'
+export const description = 'Adds various informative commands'
+export const allowedChannelTypes = ['GUILD_TEXT']
+export const isLocked = true
+export * as commands from './commands/index.js'
