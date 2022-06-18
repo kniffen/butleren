ebb3dd17f540c38b2dbe6e487ea35412c6f7e7e1
Kniffen <knifftech@gmail.com>
2022-06-18 15:37:57 +0200

    Added proxy for client during development

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+import { createProxyMiddleware } from 'http-proxy-middleware'
+
 import router from './router.js'
 
 import './guilds/index.js'
@@ -6,5 +8,9 @@ import './guilds/channels.js'
 import './guilds/roles.js'
 
 const port = process.env.APP_PORT || 5000
+
+// Proxy other request in order to use the webpack dev server for the client.
+if ('development' === process.env.NODE_ENV)
+  router.use('*', createProxyMiddleware({ target: 'http://127.0.0.1:3000', ws: true }))
 
 router.listen(port, () => console.log(`Router: Listening on port ${port}`))
