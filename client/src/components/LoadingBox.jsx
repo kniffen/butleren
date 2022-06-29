7975ff3a3bf1c77ab433fe18bc780f906c997265
Kenneth <knifftech@gmail.com>
2022-06-29 08:49:08 +0200

    Create LoadingBox.jsx

---
+++ client/src/components/LoadingBox.jsx
@@ -0,0 +1,23 @@
+import styled, { keyframes } from 'styled-components'
+
+export default function LoadingBox({ width, height }) {
+  return (
+    <StyledDiv {...{width, height}} />
+  )
+}
+
+const BackgroundAnimation = keyframes`
+  0% { background-position: 0% 50%; }
+  50% { background-position: 100% 50%; }
+  100% { background-position: 0% 50%; }
+`
+
+const StyledDiv = styled.div`
+  width: ${props => props.width || '100%'};
+  height: ${props => props.height || '100%'};
+  border-radius: 1rem;
+  background-color: var(--color-gray--600);
+  background: linear-gradient(90deg, var(--color-gray--700), var(--color-gray--600));
+  background-size: 400% 400%;
+  animation: ${BackgroundAnimation} 2s ease infinite;
+`
