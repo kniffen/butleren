ff93abd22951e1c1a9c5a4591f60b3dc18b1ed39
Kenneth <knifftech@gmail.com>
2022-06-29 12:40:06 +0200

    Update ModuleBox.jsx

--- client/src/components/ModuleBox.jsx
+++ client/src/components/ModuleBox.jsx
@@ -1,16 +1,43 @@
 import styled from 'styled-components'
 
 import Box from './Box'
+import Toggle from './Toggle'
 
-export default function ModuleBox({ id, name, description, commands, isEnabled, isLocked }) {
+export default function ModuleBox({ guild, id, name, description, commands, isEnabled, isLocked }) {
+  function handleModuleToggle(shouldBeEnabled) {
+    const init = {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({isEnabled: shouldBeEnabled})
+    }
+
+    return fetch(`/api/modules/${guild.id}/${id}`, init)
+      .then(() => shouldBeEnabled)
+      .catch((err) => {
+        console.error(err)
+        return !shouldBeEnabled
+      })
+  }
+
   return (
     <StyledModuleBox title={name}>
       <StyledModuleDescription>{description}</StyledModuleDescription>
+      <StyledToggle isDefaultChecked={isEnabled} isLocked={isLocked} onChange={handleModuleToggle}/>
     </StyledModuleBox>
   )
 }
 
-const StyledModuleBox = styled(Box)``
+const StyledModuleBox = styled(Box)`
+  position: relative;
+`
+
+const StyledToggle = styled(Toggle)`
+  position: absolute;
+  inset-block-start: 1em;
+  inset-inline-end: 1em;
+`
 
 const StyledModuleDescription = styled.p`
   margin: 0;
