ad0e041d627cc0bde41ff9dc18da6a1176009e8f
Kenneth <knifftech@gmail.com>
2022-06-29 10:31:31 +0200

    Added guild settings form

--- client/src/pages/Guild.jsx
+++ client/src/pages/Guild.jsx
@@ -1,5 +1,6 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import styled from 'styled-components'
+import moment from 'moment-timezone'
 
 import DashboardLayout from '../layouts/Dashboard'
 
@@ -7,18 +8,59 @@ import { Context } from '../Store'
 
 import Box from '../components/Box'
 import LoadingBox from '../components/LoadingBox'
+import Button from '../components/Button'
 
 export default function Guild() {
+  const [ isFormSubmiting, setIsFormSubmiting ] = useState(false)
   const { guild } = useContext(Context)
+
+  function handleSettingsSubmit(e) {
+    e.preventDefault()
+
+    setIsFormSubmiting(true)
+
+    const formData = new FormData(e.target)
+    const payload = new URLSearchParams(formData)
+
+    fetch(`/api/guilds/${guild.id}`, {method: 'PUT', body: payload})
+      .catch(console.error)
+      .finally(() => setIsFormSubmiting(false))
+  }
+
 
   if (!guild) return (
     <DashboardLayout>
+      <StyledLoadingGuildStats height="15rem" />
       <StyledLoadingGuildStats height="15rem" />
     </DashboardLayout>
   )
 
   return (
     <DashboardLayout>
+      <Box title="Settings">
+        <StyledForm onSubmit={handleSettingsSubmit}>
+          <label>
+            Nickname
+            <input name="nickname" type="text" defaultValue={guild.nickname || ''} />
+          </label>
+
+          <label>
+            Timezone
+            <select name="timezone" defaultValue={guild.timezone || 'UTC'}>
+              {moment.tz.names().map(name => <option key={name} value={name}>{name}</option>)}
+            </select>
+          </label>
+
+          <label>
+            Color
+            <br/>
+            <input type="color" name="color" defaultValue={guild.color} required/>
+          </label>
+
+          <Button type="submit" value="Save" isLoading={isFormSubmiting} />
+        </StyledForm>
+      </Box>
+
       <Box title="Guild statistics">
         <StyledGuildStatsList>
           <li>Categories: <b>{guild.categories || 0}</b></li>
@@ -30,6 +72,47 @@ export default function Guild() {
     </DashboardLayout>
   )
 }
+
+const StyledForm = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+  align-items: flex-start;
+
+  label {
+    width: 100%;
+    font-size: .85rem;
+
+    > * {
+      margin-block-start: .25em;
+    }
+  }
+
+  input[type="text"],
+  input[type="color"],
+  select {
+    width: 100%;
+    padding: .5em;
+    border: none;
+    background-color: var(--color-gray--500);
+    color: var(--color-gray--100);
+    font-size: 1rem;
+    border-radius: .25em;
+  }
+
+  input[type="color"] {
+    width: 2rem;
+    height: 2rem;
+    padding: .25em;
+    border-color: transparent;
+    cursor: pointer;
+  }
+
+  input[type="submit"] {
+    margin-block-start: 1rem;
+  }
+`
 
 const StyledLoadingGuildStats = styled(LoadingBox)`
   grid-column: span 12;
