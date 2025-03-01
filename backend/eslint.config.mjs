cd334a06cf4e863703f1b4f9e49eb7befc50b23b
Kniffen <kniffen@knifftech.org>
2025-03-01 13:21:26 +0100

    Add linting to backend

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,21 @@
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "semi": ["error", "always"],
+      "no-await-in-loop": "error",
+      "eqeqeq": ["error", "always"],
+      "yoda": ["error", "always"],
+      "prefer-const": "error",
+      "curly": ["error", "all"],
+      "@typescript-eslint/no-import-type-side-effects": "error",
+      "@typescript-eslint/explicit-function-return-type": "error",
+    }
+  }
+];
